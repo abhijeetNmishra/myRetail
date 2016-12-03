42ee75cc7b8a23362b291e19357474cefe8124c8
Abhijeet.N.Mishra <Abhijeet.N.Mishra@target.com>
2016-12-02 22:38:15 -0600

    added default notification for add to cart and pick up in store

--- client/components/ProductAddToCart/index.js
+++ client/components/ProductAddToCart/index.js
@@ -1,21 +1,46 @@
 import React from 'react';
+const ReactToastr = require("react-toastr");
 
-const ProductAddToCart = (props) => {
+const ToastContainer = ReactToastr.ToastContainer;
+const ToastMessageFactory = React.createFactory(ReactToastr.ToastMessage.animation);
 
-  const handleClick = () => {
-    console.log('added to cart');
+export default class ProductAddToCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handlePickupInStore = this.handlePickupInStore.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
   }
 
-  return (
-    <div className="row">
-      <div className="col-md-6 col-xs-6 col-xl-6 col-sm-6 col-md-6">
-        <button className="btn btn-primary pickupinstore">PICK UP IN STORE</button>
-      </div>
-      <div className="col-md-6 col-xs-6 col-xl-6 col-sm-6 col-md-6 start-xs">
-        <button className="btn btn-primary" onClick={handleClick}>ADD TO CART</button>
-      </div>
-    </div>
-  );
-};
+  handlePickupInStore() {
+    this.refs.container.success(
+      "Added to cart as pick up in store item !! ",
+      "", {
+      timeOut: 2000
+    });
+  }
 
-export default ProductAddToCart;
+  handleAddToCart() {
+    this.refs.container.success(
+      "Added to cart !! ",
+      "", {
+      timeOut: 2000
+    });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <ToastContainer ref="container"
+                          toastMessageFactory={ToastMessageFactory}
+                          className="toast-top-right" />
+        <div className="col-md-6 col-xs-6 col-xl-6 col-sm-6 col-md-6">
+          <button className="btn btn-primary pickupinstore" onClick={this.handlePickupInStore}>PICK UP IN STORE</button>
+        </div>
+        <div className="col-md-6 col-xs-6 col-xl-6 col-sm-6 col-md-6 start-xs">
+          <button className="btn btn-primary" onClick={this.handleAddToCart}>ADD TO CART</button>
+        </div>
+      </div>
+    )
+  }
+}
--- client/components/RegistryListShare/index.js
+++ client/components/RegistryListShare/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
+const ReactToastr = require("react-toastr");
 
-const RegistryListShare = (props) => {
-  return (
-    <div className="row registry-list-share">
-     <button className="btn btn-secondary btn-sm">ADD TO REGISTRY</button>
-     <button className="btn btn-secondary btn-sm">ADD TO LIST</button>
-     <button className="btn btn-secondary btn-sm">SHARE</button>
+const ToastContainer = ReactToastr.ToastContainer;
+const ToastMessageFactory = React.createFactory(ReactToastr.ToastMessage.animation);
+
+export default class RegistryListShare extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleRegistryClick = this.handleRegistryClick.bind(this);
+    this.handleShareClick = this.handleShareClick.bind(this);
+    this.handleAddToListClick = this.handleAddToListClick.bind(this);
+  }
+
+  handleRegistryClick() {
+    this.refs.container.success(
+      "Added to registry !! ",
+      "", {
+      timeOut: 2000
+    });
+  }
+
+  handleShareClick() {
+    this.refs.container.success(
+      "shared on Facebook and twitter !! ",
+      "", {
+      timeOut: 2000
+    });
+  }
+
+  handleAddToListClick() {
+    this.refs.container.success(
+      "Added to List !! ",
+      "", {
+      timeOut: 2000
+    });
+  }
+
+  render() {
+    return (
+      <div className="row registry-list-share">
+      <ToastContainer ref="container"
+                          toastMessageFactory={ToastMessageFactory}
+                          className="toast-top-right" />
+     <button className="btn btn-secondary btn-sm" onClick={this.handleRegistryClick}>ADD TO REGISTRY</button>
+     <button className="btn btn-secondary btn-sm" onClick={this.handleAddToListClick}>ADD TO LIST</button>
+     <button className="btn btn-secondary btn-sm" onClick={this.handleShareClick}>SHARE</button>
     </div>
-  );
-};
-
-export default RegistryListShare;
+    )
+  }
+}
